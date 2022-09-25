19c95540ec498f4e5f6eb483a8f949825651c4d9
Giovanna Alves de Farias <giovannaalves02@hotmail.com>
2022-09-25 13:44:04 -0300

    Criação e testes do Model

---
+++ ViewModels/LojaViewModel.cs
@@ -0,0 +1,21 @@
+namespace ShoppingMVC.ViewModels;
+
+public class LojaViewModel
+{
+    public string? Descricao { get; set; }
+    public string? Email { get; set; }
+    public int Id { get; set; }
+    public string? Nome { get; set; }
+    public string? Piso { get; set; }
+    public string? Tipo { get; set; }
+
+    public LojaViewModel(string? descricao, string? email, int id, string? nome, string? piso,  string? tipo)
+    {
+        Descricao = descricao;
+        Email = email;
+        Id = id;
+        Nome = nome;
+        Piso = piso;
+        Tipo = tipo;
+    }
+}
